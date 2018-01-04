1bdb989fb460b61fafe7c9b358095381d903688d
Jacob Sellers <jks15063@gmail.com>
2018-01-04 10:13:36 -0700

    save

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -143,14 +143,14 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(material
-                         material-light)
+   dotspacemacs-themes '(material-light
+                         material)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
    dotspacemacs-default-font '("Iosevka"
-                               :size 24
+                               :size 18
                                :weight normal
                                :width normal
                                :powerline-scale 1.0)
